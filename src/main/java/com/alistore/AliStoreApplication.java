8e1e0993f4cdfe7a270d365fa6177c760ec6205f
alivanderlande <ali.zahran@vanderlande.com>
2025-05-23 17:12:00 +0200

    Add main application class

---
+++ src/main/java/com/alistore/AliStoreApplication.java
@@ -0,0 +1,11 @@
+package com.alistore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AliStoreApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AliStoreApplication.class, args);
+    }
+}
